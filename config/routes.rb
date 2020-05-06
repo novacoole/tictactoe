0744e9c26ca31885cf0f047754558cf9bc29196f
nova <alexander.coole@protonmail.com>
2020-05-06 14:45:51 +1000

    added sessionss routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-  get 'sessions/new'
-  get 'sessions/show'
   resources :turns, only: [:create, :new, :show]
+  resources :sessions, only: [:create, :new, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
