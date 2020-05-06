a6b92ffe63da0fa3482b3f46c949ef460daa15ae
nova <alexander.coole@protonmail.com>
2020-05-06 16:04:33 +1000

    created db and migrated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,11 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user1 = User.create(name: params[:string1])
+    @user2 = User.create(name: params[:string2])
   end
 
   def show
   end
 end
+
