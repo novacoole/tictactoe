1253e2965cbb17584aca65bb0092289dfd45393e
nova <alexander.coole@protonmail.com>
2020-05-06 14:45:28 +1000

    generated sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+  get 'sessions/new'
+  get 'sessions/show'
   resources :turns, only: [:create, :new, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
