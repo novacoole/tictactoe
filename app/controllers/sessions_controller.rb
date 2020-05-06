044b3453ce29d6e798ecd7534b33d553a04930e2
nova <alexander.coole@protonmail.com>
2020-05-06 14:54:34 +1000

    added basic functionality to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,11 @@
 class SessionsController < ApplicationController
   def create
+    @session = Session.create
+    redirect_to @session
   end
 
   def new
+    @session = Session.new
   end
 
   def show
