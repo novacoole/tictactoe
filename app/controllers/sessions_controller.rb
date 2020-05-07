10bb2891f4734b217559cf4c5bb200d6cb36b211
Robert Thomas <robertthomas@outlook.com.au>
2020-05-07 15:18:42 +1000

    implimented turn creation and counting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,5 @@
 class SessionsController < ApplicationController
   
-  def create
-  end
-
-  def new
-  end
-
   def show
     @session = Session.find(params[:id])  
     @counter = @session.counter
@@ -13,17 +7,30 @@ class SessionsController < ApplicationController
     @user2 = User.find(@session.user_2)
     if @session.counter.odd?
       @player = @user1
+      @marker = true
     else 
       @player = @user2
+      @marker = false
     end
+    @turn = Turn.new(cross: @marker, users_id: @player[:id], sessions_id: @session[:id])
   end
 
   def add_a_turn
-    Turn.create(
-      cross:params[:cross], 
-      index:params[:index], 
-      users_id:params[:users_id],
-      sessions_id:params[:sessions_id]
+    @turn = Turn.create(
+      index: params[:index],
+      cross: params[:cross],
+      users_id: params[:users_id],
+      sessions_id: params[:sessions_id]
     )
+    @session = Session.find(params[:sessions_id])
+    @counter = @session.counter + 1
+    @session.update(counter: @counter)
+    redirect_to @session
   end
+
+  private
+
+  # def turn_params
+  #   params.require(:turn).permit(:index, :cross, :users_id, :sessions_id)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :turns
   post '/sessions', to: 'sessions#create', as: 'sessions_create'
+  post '/add_a_turn', to: 'sessions#add_a_turn', as: 'add_a_turn'
   resources :sessions, except: [:create, :index]
   resources :users
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#homepage'
