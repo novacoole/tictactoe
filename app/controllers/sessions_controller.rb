1ebdb5e6fb411ace2966a8740de9b8b509cff056
Robert Thomas <robertthomas@outlook.com.au>
2020-05-07 13:48:08 +1000

    implimented first turn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,5 +11,19 @@ class SessionsController < ApplicationController
     @counter = @session.counter
     @user1 = User.find(@session.user_1)
     @user2 = User.find(@session.user_2)
+    if @session.counter.odd?
+      @player = @user1
+    else 
+      @player = @user2
+    end
+  end
+
+  def add_a_turn
+    Turn.create(
+      cross:params[:cross], 
+      index:params[:index], 
+      users_id:params[:users_id],
+      sessions_id:params[:sessions_id]
+    )
   end
 end
